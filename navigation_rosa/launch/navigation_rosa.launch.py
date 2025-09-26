68307c8633ed8235e260834f16dd4f44407cc8bd
Mohamed Msallak <m.msallak@outlook.com>
2025-09-26 17:15:03 +0200

    Changes made in tql file for testing purpose, and in launch file.

--- navigation_rosa/launch/navigation_rosa.launch.py
+++ navigation_rosa/launch/navigation_rosa.launch.py
@@ -1,4 +1,4 @@
-# Copyright 2025 Gustavo Rezende Silva
+# Copyright 2024 Gustavo Rezende Silva
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -18,77 +18,61 @@ from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch.actions import IncludeLaunchDescription
-from launch.actions import ExecuteProcess
-from launch.actions import RegisterEventHandler
+from launch.conditions import LaunchConfigurationEquals
+from launch.conditions import LaunchConfigurationNotEquals
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
-from launch.event_handlers import OnProcessStart
+from launch_ros.actions import Node
+
 
 def generate_launch_description():
-	db_name = LaunchConfiguration('db_name')
-	data_path = LaunchConfiguration('data_path')
+    db_name = LaunchConfiguration('db_name')
+    data_path = LaunchConfiguration('data_path')
 
-	db_name_arg = DeclareLaunchArgument(
+    db_name_arg = DeclareLaunchArgument(
         'db_name',
-        default_value='mirte_rosa',
+        default_value='navigation_rosa',
         description='ROSA db name'
     )
 
-	pkg_mirte_rosa = get_package_share_directory('navigation_rosa')
-	data_path_ = "[{}]".format(
-		os.path.join(pkg_mirte_rosa, 'config', 'navigation_rosa.tql'))
-	data_path_arg = DeclareLaunchArgument(
-		'data_path',
-		default_value=data_path_,
-		description='typeQL file to use for data'
-	)
+    pkg_navigation_rosa = get_package_share_directory(
+        'navigation_rosa')
+    data_path_ = "[{}]".format(
+        os.path.join(pkg_navigation_rosa, 'config', 'navigation_rosa.tql'))
+    data_path_arg = DeclareLaunchArgument(
+        'data_path',
+        default_value=data_path_,
+        description='typeQL file to use for data'
+    )
 
-	pkg_rosa_bringup = get_package_share_directory(
+    pkg_rosa_kb = get_package_share_directory(
+        'rosa_kb')
+
+    pkg_rosa_bringup = get_package_share_directory(
         'rosa_bringup')
-	rosa_bringup_launch_path = os.path.join(
-		pkg_rosa_bringup,
-		'launch',
-		'rosa_bringup.launch.py')
+    rosa_bringup_launch_path = os.path.join(
+        pkg_rosa_bringup,
+        'launch',
+        'rosa_bringup.launch.py')
 
-	rosa_bringup = IncludeLaunchDescription(
-		PythonLaunchDescriptionSource(rosa_bringup_launch_path),
-		launch_arguments={
-			'data_path': data_path,
-			'database_name': db_name,
-			'force_data': 'True',
-			'force_database': 'True',
-			'infer': 'True',
-		}.items()
-	)
+    schema_path = "[{0}, {1}]".format(
+        os.path.join(pkg_rosa_kb, 'config', 'schema.tql'),
+        os.path.join(pkg_rosa_kb, 'config', 'ros_schema.tql'))
 
-	typedb_server = ExecuteProcess(
-		cmd=['typedb', 'server'],
-		name='typedb_server',
-		output='screen'
-	)
+    rosa_bringup = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(rosa_bringup_launch_path),
+        launch_arguments={
+            'schema_path': schema_path,
+            'data_path': data_path,
+            'database_name': db_name,
+            'force_data': 'True',
+            'force_database': 'True',
+            'infer': 'True',
+        }.items()
+    )
 
-	# Include another launch file after typedb starts
-	event_launch_rosa_bringup = RegisterEventHandler(
-		OnProcessStart(
-			target_action=typedb_server,
-			on_start=[rosa_bringup]
-		)
-	)
-
-	# pkg_robocup_sim_path = get_package_share_directory(
-	# 	'robocup_home_simulation')
-	# mirte_robocup_sim_launch_path = os.path.join(
-	# 	pkg_robocup_sim_path,
-	# 	'launch',
-	# 	'mirte_robocup.launch.py')
-	# mirte_robocup_launch = IncludeLaunchDescription(
-    #     PythonLaunchDescriptionSource(mirte_robocup_sim_launch_path),
-    # )
-
-	return LaunchDescription([
+    return LaunchDescription([
         db_name_arg,
         data_path_arg,
-        # mirte_robocup_launch,
-        typedb_server,
-        event_launch_rosa_bringup,
+        rosa_bringup,
     ])
